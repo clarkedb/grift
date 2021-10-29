a586bdc55768f0b5e9586162dc8f4be56e4637a0
Clark Brown <clark@neighbor.com>
2021-10-29 06:36:22 -0600

    add test for chaining mock_implementation returning MockMethod object

--- test/grift_test.rb
+++ test/grift_test.rb
@@ -15,6 +15,14 @@ class GriftTest < Minitest::Test
     target_spy.mock_restore
   end
 
+  def test_spy_on_mock_implementation_retuns_a_mock_method
+    target_mock = Grift.spy_on(Target, :full_name).mock_implementation do
+      'My full name'
+    end
+    assert_instance_of Grift::MockMethod, target_mock
+    target_mock.mock_restore
+  end
+
   def test_mock_method_raises_error
     assert_raises NotImplementedError do
       Grift.mock_method?(Target, :convince)
